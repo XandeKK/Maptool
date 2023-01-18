8e12ff30f640cfe49d8dd16459d8f5c66ef97ab3
xandekk <alexandre.dosa@gmail.com>
2023-01-18 08:29:05 -0300

    Add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/assets/javascripts/**/*.js",
+    "./app/views/**/*.{html,erb}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
