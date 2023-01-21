17a5741392841676668b94bfeb50baa2230e29f1
xandekk <alexandre.dosa@gmail.com>
2023-01-21 09:48:40 -0300

    Change dir javascript

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/assets/javascripts/**/*.js",
+    "./app/javascripts/**/*.js",
     "./app/views/**/*.{html,erb}",
     "./node_modules/flowbite/**/*.js"
   ],
