48931fa5a2075ae74c933b85eedb3d32a7a82b03
xandekk <alexandre.dosa@gmail.com>
2023-01-23 14:21:15 -0300

    Add token edit

--- app/javascripts/tokens.js
+++ app/javascripts/tokens.js
@@ -5,13 +5,83 @@ class Tokens {
 	}
 
 	get_value_property(token, property) {
-		const value = token['properties'][property];
-		if (value == undefined) value = '';
-		return value;
+		return token['properties'][property] || '';
+	}
+
+	update_token(properties, token) {
+		event.preventDefault();
+		for (let i in properties) {
+			const name = properties[i]['name'];
+			const value = document.getElementById(`${name}-${token['id']}`).value;
+
+			token['properties'][name] = value;
+		}
+
+		let token_tmp = Object.assign({}, token);
+
+		delete token_tmp.zone;
+
+		this.controller.client.send_message({
+	    "editTokenMsg": {
+	        "zoneGuid": token.zone.id,
+	        'token': token_tmp
+	    }
+		})
+		Alert.add('Updated', 'success');
 	}
 
 	create_edit(token) {
-		return document.createElement('div');
+		const properties = this.controller.maptool.get_properties(token['propertyType']);
+		const form = document.createElement('form');
+		const div = document.createElement('div');
+		const submit = document.createElement('button');
+    
+		div.id = `edit-${token['id']}`;
+		div.className = 'p-4'
+		div.role = 'tabpanel';
+		div.ariaLabelledby = `edit-${token['id']}-tab`;
+
+		form.id = `form-${token['id']}`;
+		form.className = 'md:p-5';
+		form.autocomplete = 'off';
+
+		submit.className = 'text-white bg-emerald-700 hover:bg-emerald-800 focus:ring-4 focus:outline-none focus:ring-emerald-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-emerald-600 dark:hover:bg-emerald-700 dark:focus:ring-emerald-800'
+		submit.textContent = 'Update';
+		submit.addEventListener('click', this.update_token.bind(this, properties, token))
+
+		for (let i in properties) {
+			const name = properties[i]['name'];
+
+			form.appendChild(this.create_property_field(name, token));
+		}
+
+		form.appendChild(submit);
+		div.appendChild(form);
+
+		return div;
+	}
+
+	create_property_field(property, token) {
+		const value = this.get_value_property(token, property);
+		const div = document.createElement('div');
+		const label = document.createElement('label');
+		const input = document.createElement('input');
+
+		div.className = 'mb-6';
+
+		label.className = 'block mb-2 text-sm font-medium text-gray-900 dark:text-white';
+		label.htmlFor = `${property}-${token['id']}`;
+		label.textContent = property;
+
+		input.id = `${property}-${token['id']}`;
+		input.className = 'bg-zinc-50 border border-zinc-300 text-zinc-900 text-sm rounded-lg focus:ring-emerald-500 focus:border-emerald-500 block w-full p-2.5 dark:bg-zinc-700 dark:border-zinc-600 dark:placeholder-zinc-400 dark:text-white dark:focus:ring-emerald-500 dark:focus:border-emerald-500';
+		input.type = 'text';
+		input.value = value;
+
+		div.appendChild(label);
+		div.appendChild(input);
+
+		return div;
 	}
 
 	create_control(token) {
